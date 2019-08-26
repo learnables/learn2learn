29e4b947d3863201f4ca95db23858bcfb2bc977b
seba-1511 <seba-1511@hotmail.com>
2019-08-25 21:42:12 -0700

    Move files to new folder structure.

--- learn2learn/data/__init__.py
+++ learn2learn/data/__init__.py
@@ -1,4 +1,3 @@
 #!/usr/bin/env python3
 
 from .task_generator import TaskGenerator
-from .text import NewsClassification
--- learn2learn/transforms/vision/__init__.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python3
-
-from random_discrete_rotations import RandomDiscreteRotation
