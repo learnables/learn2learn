ed58654c1bc895b4449d903d916336ad910ff78c
Praateek Mahajan <praateekm@gmail.com>
2019-08-25 22:24:20 -0700

    Revamped TaskGenerator

--- learn2learn/data/task_generator.py
+++ learn2learn/data/task_generator.py
@@ -21,15 +21,51 @@ class SampleDataset(Dataset):
         return self.data[idx], self.label[idx]
 
 
+class MetaDataset(Dataset):
+    """ It wraps a torch dataset by creating a map of target to indices.
+    This comes in handy when we want to sample elements randomly for a particular label.
+
+    Args:
+        dataset: A torch dataset
+
+    Notes:
+        For l2l to work its important that the dataset returns a (data, target) tuple.
+        If your dataset doesn't return that, it should be trivial to wrap your dataset
+        with another class to do that.
+        #TODO : Add example for wrapping a non standard l2l dataset
+    """
+
+    def __init__(self, dataset: Dataset):
+        self.dataset = dataset
+        self.labels_to_indices = self.get_dict_of_labels_to_indices()
+        self.labels = list(self.labels_to_indices.keys())
+
+    def __getitem__(self, item):
+        return self.dataset[item]
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def get_dict_of_labels_to_indices(self):
+        """ Iterates over the entire dataset and creates a map of target to indices.
+
+        Returns: A dict with key as the label and value as list of indices.
+
+        """
+
+        classes_to_indices = defaultdict(list)
+        for i in range(len(self.dataset)):
+            classes_to_indices[self.dataset[i][1]].append(i)
+        return classes_to_indices
+
+
 class LabelEncoder:
     def __init__(self, classes):
         """ Encodes a list of classes into indices, starting from 0.
 
         Args:
-            classes: list, tuple of classes
+            classes: List of classes
         """
-        # shuffle all our classes
-        classes = np.random.shuffle(classes)
         assert len(set(classes)) == len(classes), "Classes contains duplicate values"
         self.class_to_idx = dict()
         self.idx_to_class = dict()
@@ -39,75 +75,23 @@ class LabelEncoder:
 
 
 class TaskGenerator:
-    def __init__(self, dataset: Dataset, ways: int = 3, split=False, test_size=3, test_classes=[]):
+    def __init__(self, dataset: MetaDataset, classes: list, ways: int = 3):
         """
 
         Args:
-            dataset: should be a Dataset that returns (data, target)
+            dataset: should be a MetaDataset
+            classes: List of classes to sample from
             ways: number of labels to sample from
-            split: If set to True, it will split the labels into train and test.
-            test_size: If split is set to True, then it uses test_size samples in test
-
         """
 
+        # TODO : Add conditional check to work even when dataset isn't MetaDataset and a torch.Dataset
+        #  then also it should work
         self.dataset = dataset
         self.ways = ways
-        self.split = split
+        self.classes = classes
+        assert len(classes) >= ways, ValueError("Ways are more than the number of classes available")
 
-        self.target_to_indices = self.get_dict_of_target_to_indices()
-
-        self.classes = list(self.target_to_indices.keys())
-        self.train_classes = list()
-        self.test_classes = test_classes
-
-        if self.split:
-            self.split_datasets(n=test_size)
-
-    def __len__(self):
-        return len(self.dataset)
-
-    def split_datasets(self, n):
-        """ This method would randomly select n classes to belong in test classes.
-        This way we can create two TaskGenerators and measure how easily
-        we learn on data we've never seen before.
-
-        """
-        if self.split:
-            if len(self.test_classes) > 0:
-                self.test_classes = self.test_classes
-            else:
-                self.test_classes = np.random.choice(self.classes, size=n, replace=False)
-
-            self.train_classes = list(set(self.classes) - set(self.test_classes))
-
-    def get_dict_of_target_to_indices(self):
-        """ Iterates over the entire dataset and creates a map of target to indices.
-
-        Returns: A dict with key as the label and value as list of indices.
-
-        """
-        target_to_indices = defaultdict(list)
-        for i in range(len(self.dataset)):
-            target_to_indices[self.dataset[i][1]].append(i)
-        return target_to_indices
-
-    def get_random_label_pair(self, sample_from_train):
-        """ Creates a random label tuple.
-
-        Selects `ways` number of random labels from the set of labels.
-
-        Returns: list of labels
-
-        """
-        if self.split:
-            if sample_from_train:
-                return np.random.choice(self.train_classes, size=self.ways, replace=False)
-            else:
-                return np.random.choice(self.test_classes, size=self.ways, replace=False)
-        else:
-            return np.random.choice(self.classes, size=self.ways, replace=False)
-
-    def sample(self, shots: int = 5, classes_to_sample=None, sample_from_train=True):
+    def sample(self, shots: int = 5):
         """ Returns a dataset and the labels that we have sampled.
 
         The dataset is of length `shots * ways`.
@@ -120,13 +104,12 @@ class TaskGenerator:
         Returns: Dataset, list(labels)
 
         """
-        if classes_to_sample is None:
-            classes_to_sample = self.get_random_label_pair(sample_from_train)
+        classes_to_sample = np.random.choice(self.classes, size=self.ways, replace=False)
         label_encoding = LabelEncoder(classes_to_sample)
         data_indices = []
         classes = []
         for _class in classes_to_sample:
-            data_indices.extend(np.random.choice(self.target_to_indices[_class], shots, replace=False))
+            data_indices.extend(np.random.choice(self.dataset.labels_to_indices[_class], shots, replace=False))
             classes.extend(np.full(shots, fill_value=label_encoding.class_to_idx[_class]))
 
         data = [self.dataset[idx][0] for idx in data_indices]
