d425a95430369c2d8283f679146c569956b77a47
Praateek Mahajan <praateekm@gmail.com>
2019-09-02 22:06:28 -0700

    line addition and renaming vars

--- tests/unit/util_tests.py
+++ tests/unit/util_tests.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 import unittest
+
 import torch as th
-import learn2learn as l2l 
+
+import learn2learn as l2l
 
 
 class Model(th.nn.Module):
@@ -10,10 +12,10 @@ class Model(th.nn.Module):
     def __init__(self):
         super().__init__()
         self.model = th.nn.Sequential(
-                          th.nn.Linear(4, 64),
-                          th.nn.Tanh(),
-                          th.nn.Linear(64, 2)
-                          )
+            th.nn.Linear(4, 64),
+            th.nn.Tanh(),
+            th.nn.Linear(64, 2)
+        )
 
     def forward(self, x):
         return self.model(x)
@@ -23,59 +25,57 @@ class UtilTests(unittest.TestCase):
 
     def setUp(self):
         self.model = Model()
-        self.loss = th.nn.MSELoss()
-        self.input = th.tensor([[0.,1.,2.,3.]])
-
+        self.loss_func = th.nn.MSELoss()
+        self.input = th.tensor([[0., 1., 2., 3.]])
 
     def tearDown(self):
         pass
 
     def test_module_clone(self):
-        x = self.model(self.input)
-        y = self.loss(x, th.tensor([[0.,0.]]))
+        output = self.model(self.input)
+        loss = self.loss_func(output, th.tensor([[0., 0.]]))
 
-        gradients = th.autograd.grad(y,
-                         self.model.parameters(),
-                         retain_graph=True,
-                         create_graph=True)
+        gradients = th.autograd.grad(loss,
+                                     self.model.parameters(),
+                                     retain_graph=True,
+                                     create_graph=True)
 
-        clone = l2l.clone_module(self.model) 
+        cloned_model = l2l.clone_module(self.model)
 
         for param, gradient in zip(self.model.parameters(), gradients):
             param = param - .01 * gradient
 
-        x = clone(self.input)
-        y = self.loss(x, th.tensor([[0.,0.]]))
+        output = cloned_model(self.input)
+        loss = self.loss_func(output, th.tensor([[0., 0.]]))
 
-        gradients = th.autograd.grad(y,
-                         clone.parameters(),
-                         retain_graph=True,
-                         create_graph=True)
+        gradients = th.autograd.grad(loss,
+                                     cloned_model.parameters(),
+                                     retain_graph=True,
+                                     create_graph=True)
 
-        for param, gradient in zip(clone.parameters(), gradients):
+        for param, gradient in zip(cloned_model.parameters(), gradients):
             param = param - .01 * gradient
 
-        for a,b in zip(self.model.parameters(), clone.parameters()):
-            assert th.equal(a,b)
+        for a, b in zip(self.model.parameters(), cloned_model.parameters()):
+            assert th.equal(a, b)
 
-    
     def test_module_detach(self):
         x = self.model(self.input)
-        y = self.loss(x, th.tensor([[0.,0.]]))
+        y = self.loss_func(x, th.tensor([[0., 0.]]))
 
         gradients = th.autograd.grad(y,
-                         self.model.parameters(),
-                         retain_graph=True,
-                         create_graph=True)
+                                     self.model.parameters(),
+                                     retain_graph=True,
+                                     create_graph=True)
 
-        l2l.detach_module(self.model) 
+        l2l.detach_module(self.model)
         severed = self.model
 
         for param, gradient in zip(self.model.parameters(), gradients):
             param = param - .01 * gradient
 
         x = severed(self.input)
-        y = self.loss(x, th.tensor([[0.,0.]]))
+        y = self.loss_func(x, th.tensor([[0., 0.]]))
 
         fail = False
         try:
@@ -94,7 +94,7 @@ class UtilTests(unittest.TestCase):
 
     def test_distribution_detach(self):
         pass
-        
-        
+
+
 if __name__ == '__main__':
     unittest.main()
