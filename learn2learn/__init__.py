8ea1465d2c240294e6f1eab5b44f4408fd91b16f
seba-1511 <seba-1511@hotmail.com>
2019-09-07 13:17:55 -0700

    Add environment License

--- learn2learn/__init__.py
+++ learn2learn/__init__.py
@@ -8,4 +8,4 @@ from . import text
 from . import vision
 from ._version import __version__
 from .algorithms import MAML, MetaSGD, magic_box
-from .utils import *  # Needs to be imported first.
+from .utils import *
