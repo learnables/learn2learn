21faad491f7648a3ac3fda393a49631b5849f54c
Ian Bunner <bunner@usc.edu>
2019-09-10 17:49:14 -0700

    Docs for RL Environments. (#47)
    * update getting_started to contain intro to l2l and meta-learning
    
    * update mkdocs and pydocmd config files to restore equation rendering and fix equations in getting_started tutorial
    
    * update getting_started
    
    * pydocmd builds on top of mkdocs
    
    * clean up notation and add installation notes
    
    * docstrings for meta_env and particles
    
    * docstrings for mujoco envs
    
    * update mujoco env docstrings
    
    * fix mujoco docstrings
    
    * update meta_env docstring
    
    * docstring for asyncVec
    
    * add link to source in async docstring
    
    * add link to source in async docstring
    
    * remove newline between references

--- learn2learn/gym/envs/mujoco/humanoid_direction.py
+++ learn2learn/gym/envs/mujoco/humanoid_direction.py
@@ -14,6 +14,31 @@ def mass_center(model, sim):
 
 
 class HumanoidDirectionEnv(MetaEnv, MujocoEnv, gym.utils.EzPickle):
+    """
+    [[Source]](https://github.com/learnables/learn2learn/blob/master/learn2learn/gym/envs/mujoco/humanoid_direction.py)
+
+    **Description**
+
+    This environment requires the humanoid to learn to run in a random direction in the
+    XY plane. At each time step the humanoid receives a signal composed of a
+    control cost and a reward equal to its average velocity in the target direction.
+    The tasks are 2d-arrays sampled uniformly along the unit circle.
+    The target direction is indicated by the vector from the origin to the sampled point. 
+    The velocity is calculated as the distance (in the target direction) of the humanoid's torso
+    position before and after taking the specified action divided by a small value dt.
+    A small positive bonus is added to the reward to stop the humanoid from 
+    prematurely ending the episode.
+
+    **Credit**
+
+    Adapted from Jonas Rothfuss' implementation.
+
+    **References**
+
+    1. Finn et al. 2017. "Model-Agnostic Meta-Learning for Fast Adaptation of Deep Networks." arXiv [cs.LG].
+    2. Rothfuss et al. 2018. "ProMP: Proximal Meta-Policy Search." arXiv [cs.LG].
+
+    """
 
     def __init__(self, task=None):
         MetaEnv.__init__(self, task)
