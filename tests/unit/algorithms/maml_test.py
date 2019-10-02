df9edefc98345dfab67523036514895ec83cf49f
Praateek Mahajan <praateekm@gmail.com>
2019-10-02 10:27:56 -0700

    Added formatting and moved stuff to setupClass

--- tests/unit/algorithms/maml_test.py
+++ tests/unit/algorithms/maml_test.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import unittest
+
 import torch as th
+
 import learn2learn as l2l
 
 NUM_INPUTS = 7
@@ -12,42 +14,38 @@ EPSILON = 1e-8
 
 
 def close(x, y):
-    return (x-y).norm(p=2) <= EPSILON
+    return (x - y).norm(p=2) <= EPSILON
 
 
 class TestMAMLAlgorithm(unittest.TestCase):
 
-    def setUp(self):
-        pass
+    @classmethod
+    def setUpClass(cls):
+        cls.model = th.nn.Sequential(th.nn.Linear(INPUT_SIZE, HIDDEN_SIZE),
+                                     th.nn.ReLU(),
+                                     th.nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE),
+                                     th.nn.Sigmoid(),
+                                     th.nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE),
+                                     th.nn.Softmax())
+
+        cls.model.register_buffer('dummy_buf', th.zeros(1, 2, 3, 4))
 
     def tearDown(self):
         pass
 
     def test_clone_module(self):
         for first_order in [False, True]:
-            model = th.nn.Sequential(th.nn.Linear(INPUT_SIZE, HIDDEN_SIZE),
-                                     th.nn.ReLU(),
-                                     th.nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE),
-                                     th.nn.Sigmoid(),
-                                     th.nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE),
-                                     th.nn.Softmax())
-            maml = l2l.algorithms.MAML(model,
+            maml = l2l.algorithms.MAML(self.model,
                                        lr=INNER_LR,
                                        first_order=first_order)
             X = th.randn(NUM_INPUTS, INPUT_SIZE)
-            ref = model(X)
+            ref = self.model(X)
             for clone in [maml.clone(), maml.clone()]:
                 out = clone(X)
                 self.assertTrue(close(ref, out))
 
     def test_graph_connection(self):
-        model = th.nn.Sequential(th.nn.Linear(INPUT_SIZE, HIDDEN_SIZE),
-                                 th.nn.ReLU(),
-                                 th.nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE),
-                                 th.nn.Sigmoid(),
-                                 th.nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE),
-                                 th.nn.Softmax())
-        maml = l2l.algorithms.MAML(model,
+        maml = l2l.algorithms.MAML(self.model,
                                    lr=INNER_LR,
                                    first_order=False)
         X = th.randn(NUM_INPUTS, INPUT_SIZE)
@@ -55,18 +53,12 @@ class TestMAMLAlgorithm(unittest.TestCase):
         clone = maml.clone()
         out = clone(X)
         out.norm(p=2).backward()
-        for p in model.parameters():
+        for p in self.model.parameters():
             self.assertTrue(hasattr(p, 'grad'))
             self.assertTrue(p.grad.norm(p=2).item() > 0.0)
 
     def test_adaptation(self):
-        model = th.nn.Sequential(th.nn.Linear(INPUT_SIZE, HIDDEN_SIZE),
-                                 th.nn.ReLU(),
-                                 th.nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE),
-                                 th.nn.Sigmoid(),
-                                 th.nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE),
-                                 th.nn.Softmax())
-        maml = l2l.algorithms.MAML(model,
+        maml = l2l.algorithms.MAML(self.model,
                                    lr=INNER_LR,
                                    first_order=False)
         X = th.randn(NUM_INPUTS, INPUT_SIZE)
@@ -76,7 +68,7 @@ class TestMAMLAlgorithm(unittest.TestCase):
         new_loss = clone(X).norm(p=2)
         self.assertTrue(loss >= new_loss)
         new_loss.backward()
-        for p in model.parameters():
+        for p in self.model.parameters():
             self.assertTrue(hasattr(p, 'grad'))
             self.assertTrue(p.grad.norm(p=2).item() > 0.0)
 
