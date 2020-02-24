2d0e7f92d9a4dfe23142899b33e66e7ec3adbc59
seba-1511 <seba-1511@hotmail.com>
2020-02-23 19:16:32 -0800

    Update install.

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ from setuptools import (
     Extension
 )
 from Cython.Build import cythonize
+from Cython.Distutils import build_ext
 
 # Parses version number: https://stackoverflow.com/a/7071358
 VERSIONFILE = 'learn2learn/_version.py'
@@ -26,23 +27,25 @@ else:
 include_dirs = []
 compiler_directives = {'language_level': 3, 'embedsignature': True, 'binding': True}
 extensions = [
-    Extension(name='learn2learn.data.task_dataset',
-              sources=['learn2learn/data/task_dataset.pyx']), 
     Extension(name='learn2learn.data.meta_dataset',
               sources=['learn2learn/data/meta_dataset.pyx']), 
+    Extension(name='learn2learn.data.task_dataset',
+              sources=['learn2learn/data/task_dataset.pyx']), 
     Extension(name='learn2learn.data.transforms',
               sources=['learn2learn/data/transforms.pyx']), 
 ]
+
 setup(
       name='learn2learn',
       ext_modules=cythonize(extensions, compiler_directives=compiler_directives),
       include_dirs=include_dirs,
+      cmdclass={'build_ext': build_ext},
 )
 
 # Installs the package
 install(
     name='learn2learn',
-    packages='learn2learn',
+    packages=find_packages(),
     version=VERSION,
     description='PyTorch Meta-Learning Framework for Researchers',
     long_description=open('README.md').read(),
@@ -54,7 +57,7 @@ install(
     license='MIT',
     classifiers=[],
     scripts=[],
-    setup_requires=["cython>=0.28.5"],
+    setup_requires=['cython>=0.28.5',],
     install_requires=[
         'numpy>=1.15.4',
         'gym>=0.14.0',
