1557d15d6deded29717582e5703010cf908d5afa
seba-1511 <seba-1511@hotmail.com>
2020-02-29 18:44:25 -0800

    Rewrite results README for MAML/ProtoNets.

--- examples/vision/README.md
+++ examples/vision/README.md
@@ -2,75 +2,70 @@
 
 This directory contains meta-learning examples and reproductions for common computer vision benchmarks.
 
-## MAML on Omniglot
+## MAML
 
-This file reproduces MAML on the Omniglot dataset.
-The CNN results can be obtained by swapping `OmniglotFC` with `OmniglotCNN`.
-As far as we know, the hyper-parameters should also replicate the 5-ways 5-shots setting.
+The following files reproduce MAML on the Omniglot and *mini*-ImageNet datasets.
+The FOMAML results can be obtained by setting `first_order=True` in the `MAML` wrapper.
+On Omniglot, the CNN results can be obtained by swapping `OmniglotFC` with `OmniglotCNN`.
 
-Note that the original MAML paper trains with 1 fast adaptation step, but tests with 3 steps.
+* [maml_omniglot.py](https://github.com/learnables/learn2learn/blob/master/examples/vision/maml_omniglot.py) - MAML on the Omniglot dataset with a fully-connected network.
+* [maml_miniimagenet.py](https://github.com/learnables/learn2learn/blob/master/examples/vision/maml_miniimagenet.py) - MAML on the *mini*-ImageNet dataset with the standard convolutional network.
+
+Note that the original MAML paper trains with 5 fast adaptation step, but tests with 10 steps.
 This implementation only provides the training code.
 
-**Results**
+When adapting the code to different datasets, we obtained the following results.
+Only the fast-adaptation learning rate needs a bit of tuning, and good values usually lie in a 0.5-2x range of the original value.
 
-| Setup                         | Original      | learn2learn  |
-| ----------------------------- | ------------- | ------------ |
-| FC, 1 shot, 5 ways, 1 step    | 89.7%         | 88.9%        |
-| CNN, 1 shot, 5 ways, 1 step   | 98.7%         | 99.1%        |
+| Dataset       | Architecture | Ways | Shots | Original | learn2learn |
+|---------------|--------------|------|-------|----------|-------------|
+| Omniglot      | FC           | 5    | 1     | 89.7%    | 88.9%       |
+| Omniglot      | CNN          | 5    | 1     | 98.7%    | 99.1%       |
+| mini-ImageNet | CNN          | 5    | 1     | 48.7%    | 48.3%       |
+| mini-ImageNet | CNN          | 5    | 5     | 63.1%    | 65.4%       |
+| CIFAR-FS      | CNN          | 5    | 5     | 71.5%    | 73.6%       |
+| FC100         | CNN          | 5    | 5     | n/a      | 49.0%       |
 
 **Usage**
+
+Manually edit the respective files, and then run
 
 ~~~shell
 python examples/vision/maml_omniglot.py
 ~~~
 
-**Colab** Follow [this link](https://colab.research.google.com/drive/1N1vtHAPJBaJO1wD30b_fnwPOWDorm_gP) to see this example live on Google Colab.
-
-## MAML on mini-ImageNet
-
-This file reproduces MAML on the *mini*-ImageNet dataset.
-The FOMAML results can be obtained by setting `first_order=True` in the `MAML` wrapper.
-
-Note that the original MAML paper trains with 5 fast adaptation step, but tests with 10 steps.
-This implementation only provides the training code.
-
-**Results**
-
-| Setup                         | Original      | learn2learn  |
-| ----------------------------- | ------------- | ------------ |
-| CNN, 1 shot, 5 ways, 5 step   | 48.7%         | 48.3%        |
-| CNN, 5 shot, 5 ways, 5 step   | 63.1%         | 64.8%        |
-
-**Usage**
+or
 
 ~~~shell
 python examples/vision/maml_miniimagenet.py
 ~~~
 
-**Colab** Follow [this link](https://colab.research.google.com/drive/1NIWbMYM3CercnoNMmlhre7hsJ8wMtAry) to see this example live on Google Colab.
+## Prototypical Networks
 
-
-## ProtoNets on mini-ImageNet
-
-This file reproduces protonet on the *mini*-ImageNet dataset.
+This file [protonet_miniimagenet.py](https://github.com/learnables/learn2learn/blob/master/examples/vision/protonet_miniimagenet.py) reproduces protonet on the *mini*-ImageNet dataset.
 
 This implementation provides training and testing code.
 
-**Results**
 
-| Setup                         | Original      | learn2learn  |
-| ----------------------------- | ------------- | ------------ |
-| CNN, 1 shot, 5 ways           | 49.4%         | 49.1%        |
-| CNN, 5 shot, 5 ways           | 68.2%         | 66.5%        |
+| Dataset       | Architecture | Ways | Shots | Original | learn2learn |
+|---------------|--------------|------|-------|----------|-------------|
+| mini-ImageNet | CNN          | 5    | 1     | 49.4%    | 49.1%       |
+| mini-ImageNet | CNN          | 5    | 5     | 68.2%    | 66.5%       |
+
 
 **Usage**
 
-For 1 shot 5 ways
+For 1 shot 5 ways:
 ~~~shell
 python examples/vision/protonet_miniimagenet.py
 ~~~
 
-For 5 shot 5 ways
+For 5 shot 5 ways:
 ~~~shell
 python examples/vision/protonet_miniimagenet.py --shot 5 --train-way 20
 ~~~
+
+## ANIL
+
+## SimpleShot
+
--- examples/vision/simpleshot_cifarfs.py
+++ examples/vision/simpleshot_cifarfs.py
@@ -3,7 +3,7 @@
 """
 Meta-Learning with SimpleShot.
 
-Training: classical supervised learning.
+Training: classical multiclass supervised learning.
 Testing: Use centered and L2-normalized features.
 """
 
@@ -16,19 +16,41 @@ from torch import nn
 
 import learn2learn as l2l
 
-from statistics import mean
-from copy import deepcopy
-from learn2learn.data.transforms import NWays, KShots, LoadData, RemapLabels, ConsecutiveLabels
+from learn2learn.data.transforms import FusedNWaysKShots, LoadData, RemapLabels, ConsecutiveLabels
+from torchvision.transforms import ToTensor
 
 
-class Lambda(nn.Module):
+class Lambda(torch.nn.Module):
 
-    def __init__(self, fn):
+    def __init__(self, lmb):
         super(Lambda, self).__init__()
-        self.fn = fn
+        self.lmb = lmb
 
     def forward(self, x):
-        return self.fn(x)
+        return self.lmb(x)
+
+
+class CifarFSCNN(torch.nn.Module):
+
+    def __init__(self, output_size):
+        super(CifarFSCNN, self).__init__()
+        self.features = torch.nn.Sequential(
+            l2l.vision.models.ConvBase(output_size=64,
+                                       hidden=64,
+                                       channels=3,
+                                       layers=4,
+                                       max_pool=True,
+                                       max_pool_factor=1.0,
+                                       ),
+            Lambda(lambda x: x.view(-1, 256))
+        )
+        self.head = torch.nn.Linear(256, output_size, bias=True)
+        l2l.vision.models.maml_init_(self.head)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.head(x)
+        return x
 
 
 def pairwise_distances_logits(a, b):
@@ -44,7 +66,7 @@ def accuracy(predictions, targets):
     return (predictions == targets).sum().float() / targets.size(0)
 
 
-def fast_adapt(batch, model, x_mean, loss, query_num, shots, ways, num_clusters=1, metric=None, device=None):
+def fast_adapt(batch, model, x_mean, loss, query_num, shots, ways, metric=None, device=None):
     if metric is None:
         metric = pairwise_distances_logits
     if device is None:
@@ -52,7 +74,7 @@ def fast_adapt(batch, model, x_mean, loss, query_num, shots, ways, num_clusters=
     data, labels = batch
     data = data.to(device)
     labels = labels.to(device)
-    n_items = num_clusters * shots * ways
+    n_items = shots * ways
 
     # Sort data samples by labels
     sort = torch.sort(labels)
@@ -60,16 +82,18 @@ def fast_adapt(batch, model, x_mean, loss, query_num, shots, ways, num_clusters=
     labels = labels.squeeze(0)[sort.indices].squeeze(0)
 
     # Compute support and query embeddings
-    embeddings = model(data)
+    embeddings = model(data) - x_mean
     embeddings = embeddings / embeddings.norm(p=2, dim=1, keepdim=True)
-    support_indices = torch.zeros(data.size(0)).byte()
-    selection = torch.arange(ways) * (shots + query_num)
+    support_indices = np.zeros(data.size(0), dtype=bool)
+    selection = np.arange(ways) * (shots + query_num)
     for offset in range(shots):
-        support_indices[selection + offset] = 1
+        support_indices[selection + offset] = True
+    query_indices = torch.from_numpy(~support_indices)
+    support_indices = torch.from_numpy(support_indices)
     support = embeddings[support_indices]
     support = support.reshape(ways, shots, -1).mean(dim=1)
-    query = embeddings[1 - support_indices]
-    labels = labels[1 - support_indices].long()
+    query = embeddings[query_indices]
+    labels = labels[query_indices].long()
 
     logits = pairwise_distances_logits(query, support)
     loss = loss(logits, labels)
@@ -78,12 +102,12 @@ def fast_adapt(batch, model, x_mean, loss, query_num, shots, ways, num_clusters=
 
 
 def main(
-        test_ways=10,
-        test_shots=1,
+        test_ways=5,
+        test_shots=5,
         test_queries=5,
-        lr=0.001,
-        meta_bsz=32,
-        iters=2500,
+        lr=0.003,
+        meta_bsz=64,
+        iters=200,
         cuda=1,
         seed=42,
 ):
@@ -99,24 +123,28 @@ def main(
         device = torch.device('cuda')
 
     # Create Datasets
-    train_dataset = l2l.vision.datasets.MiniImagenet(root='~/data', mode='train')
-    valid_dataset = l2l.vision.datasets.MiniImagenet(root='~/data', mode='validation')
-    test_dataset = l2l.vision.datasets.MiniImagenet(root='~/data', mode='test')
+    train_dataset = l2l.vision.datasets.CIFARFS(root='~/data',
+                                                mode='train',
+                                                transform=ToTensor())
+    valid_dataset = l2l.vision.datasets.CIFARFS(root='~/data',
+                                                mode='validation',
+                                                transform=ToTensor())
+    test_dataset = l2l.vision.datasets.CIFARFS(root='~/data',
+                                               mode='test',
+                                               transform=ToTensor())
     train_dataset = l2l.data.MetaDataset(train_dataset)
     valid_dataset = l2l.data.MetaDataset(valid_dataset)
     test_dataset = l2l.data.MetaDataset(test_dataset)
 
     train_transforms = [
-        NWays(train_dataset, test_ways),
-        KShots(train_dataset, test_queries + test_shots),
+        FusedNWaysKShots(train_dataset, n=test_ways, k=test_queries+test_shots),
         LoadData(train_dataset),
         RemapLabels(train_dataset),
     ]
     train_tasks = l2l.data.TaskDataset(train_dataset, task_transforms=train_transforms)
 
     valid_transforms = [
-        NWays(valid_dataset, test_ways),
-        KShots(valid_dataset, test_queries + test_shots),
+        FusedNWaysKShots(valid_dataset, n=test_ways, k=test_queries+test_shots),
         LoadData(valid_dataset),
         RemapLabels(valid_dataset),
     ]
@@ -125,8 +153,7 @@ def main(
                                        num_tasks=200)
 
     test_transforms = [
-        NWays(test_dataset, test_ways),
-        KShots(test_dataset, test_queries + test_shots),
+        FusedNWaysKShots(test_dataset, n=test_ways, k=test_queries+test_shots),
         LoadData(test_dataset),
         RemapLabels(test_dataset),
     ]
@@ -139,12 +166,12 @@ def main(
                                                shuffle=True)
 
     # Create model
-    model = l2l.vision.models.MiniImagenetCNN(64)
+    model = CifarFSCNN(output_size=64)
     model.to(device)
-    features = torch.nn.Sequential(model.base, Lambda(lambda x: x.view(-1, 800)))
+    features = model.features
 
     # Setup optimization
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=0.001)
     loss = nn.CrossEntropyLoss(reduction='mean')
 
     for iteration in range(iters):
@@ -183,7 +210,6 @@ def main(
                                                                    test_queries,
                                                                    test_shots,
                                                                    test_ways,
-                                                                   num_clusters=1,
                                                                    device=device)
                 meta_train_error += evaluation_error.item()
                 meta_train_accuracy += evaluation_accuracy.item()
@@ -198,7 +224,6 @@ def main(
                                                                    test_queries,
                                                                    test_shots,
                                                                    test_ways,
-                                                                   num_clusters=1,
                                                                    device=device)
                 meta_valid_error += evaluation_error.item()
                 meta_valid_accuracy += evaluation_accuracy.item()
@@ -212,7 +237,6 @@ def main(
                                                                    test_queries,
                                                                    test_shots,
                                                                    test_ways,
-                                                                   num_clusters=1,
                                                                    device=device)
                 meta_test_error += evaluation_error.item()
                 meta_test_accuracy += evaluation_accuracy.item()
