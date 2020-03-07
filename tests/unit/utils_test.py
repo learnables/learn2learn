b3b1dbabf201a05c2572ebd629cc6c26e0d27f5d
seba-1511 <seba-1511@hotmail.com>
2020-03-07 15:48:26 -0800

    Swap deepcopy for a custom shallow copy in clone_module().

--- tests/unit/utils_test.py
+++ tests/unit/utils_test.py
@@ -1,8 +1,34 @@
 #!/usr/bin/env python3
 
 import unittest
+import copy
 import torch
 import learn2learn as l2l
+
+
+def ref_clone_module(module):
+    # First, create a copy of the module.
+    clone = copy.deepcopy(module)
+
+    # Second, re-write all parameters
+    if hasattr(clone, '_parameters'):
+        for param_key in module._parameters:
+            if module._parameters[param_key] is not None:
+                cloned = module._parameters[param_key].clone()
+                clone._parameters[param_key] = cloned
+
+    # Third, handle the buffers if necessary
+    if hasattr(clone, '_buffers'):
+        for buffer_key in module._buffers:
+            if clone._buffers[buffer_key] is not None and \
+                    clone._buffers[buffer_key].requires_grad:
+                clone._buffers[buffer_key] = module._buffers[buffer_key].clone()
+
+    # Then, recurse for each submodule
+    if hasattr(clone, '_modules'):
+        for module_key in clone._modules:
+            clone._modules[module_key] = ref_clone_module(module._modules[module_key])
+    return clone
 
 
 class Model(torch.nn.Module):
@@ -33,7 +59,7 @@ class UtilTests(unittest.TestCase):
         for param, gradient in zip(model.parameters(), gradients):
             param.data.sub_(0.01 * gradient)
 
-    def test_module_clone(self):
+    def test_clone_module_basics(self):
         original_output = self.model(self.input)
         original_loss = self.loss_func(original_output, torch.tensor([[0., 0.]]))
         original_gradients = torch.autograd.grad(original_loss,
@@ -55,7 +81,44 @@ class UtilTests(unittest.TestCase):
         self.optimizer_step(cloned_model, cloned_gradients)
 
         for a, b in zip(self.model.parameters(), cloned_model.parameters()):
-            assert torch.equal(a, b)
+            self.assertTrue(torch.equal(a, b))
+
+    def test_clone_module_models(self):
+        ref_models = [l2l.vision.models.OmniglotCNN(10),
+                  l2l.vision.models.MiniImagenetCNN(10)]
+        l2l_models = [copy.deepcopy(m) for m in ref_models]
+        inputs = [torch.randn(5, 1, 28, 28), torch.randn(5, 3, 84, 84)]
+
+
+        # Compute reference gradients
+        ref_grads = []
+        for model, X in zip(ref_models, inputs):
+            for iteration in range(10):
+                model.zero_grad()
+                clone = ref_clone_module(model)
+                out = clone(X)
+                out.norm(p=2).backward()
+                self.optimizer_step(model, [p.grad for p in model.parameters()])
+                ref_grads.append([p.grad.clone().detach() for p in model.parameters()])
+
+        # Compute cloned gradients
+        l2l_grads = []
+        for model, X in zip(l2l_models, inputs):
+            for iteration in range(10):
+                model.zero_grad()
+                clone = l2l.clone_module(model)
+                out = clone(X)
+                out.norm(p=2).backward()
+                self.optimizer_step(model, [p.grad for p in model.parameters()])
+                l2l_grads.append([p.grad.clone().detach() for p in model.parameters()])
+
+        # Compare gradients and model parameters
+        for ref_g, l2l_g in zip(ref_grads, l2l_grads):
+            for r_g, l_g in zip(ref_g, l2l_g):
+                self.assertTrue(torch.equal(r_g, l_g))
+        for ref_model, l2l_model in zip(ref_models, l2l_models):
+            for ref_p, l2l_p in zip(ref_model.parameters(), l2l_model.parameters()):
+                self.assertTrue(torch.equal(ref_p, l2l_p))
 
     def test_module_detach(self):
         original_output = self.model(self.input)
