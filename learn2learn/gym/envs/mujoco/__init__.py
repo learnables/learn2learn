9ff3ab7b022e4eb74c4c1a7ea843de10f64657b0
Séb Arnold <seba-1511@hotmail.com>
2020-04-20 13:45:31 -0400

    Make Mujoco optional to generate documentation. (#137)
    * Remove mandatory import for Mujoco envs. (docs)
    
    * Fix linting and import errors.
    
    * Update changelog.

--- learn2learn/gym/envs/mujoco/__init__.py
+++ learn2learn/gym/envs/mujoco/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from .dummy_mujoco_env import MujocoEnv
 from .ant_direction import AntDirectionEnv
 from .ant_forward_backward import AntForwardBackwardEnv
 from .halfcheetah_forward_backward import HalfCheetahForwardBackwardEnv
