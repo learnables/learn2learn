0ef1547adb7123c29c5a933a1f6e125580671ef7
seba-1511 <seba-1511@hotmail.com>
2020-07-01 14:16:38 -0700

    Add docs for GBML.

--- learn2learn/algorithms/gbml.py
+++ learn2learn/algorithms/gbml.py
@@ -7,9 +7,61 @@ import learn2learn as l2l
 class GBML(torch.nn.Module):
     """
 
-    TODO:
-        * Support for allow_unused
-        * Support for allow_nograd
+    [[Source]](https://github.com/learnables/learn2learn/blob/master/learn2learn/algorithms/gbml.py)
+
+    **Description**
+
+    General wrapper for gradient-based meta-learning implementations.
+
+    A variety of algorithms can simply be implemented by changing the kind
+    of `transform` used during fast-adaptation.
+    For example, if the transform is `Scale` we recover Meta-SGD [2] with `adapt_transform=False`
+    and Alpha MAML [4] with `adapt_transform=True`.
+    If the transform is a Kronecker-factored module (e.g. neural network, or linear), we recover 
+    KFO from [5].
+
+    **Arguments**
+
+    * **module** (Module) - Module to be wrapped.
+    * **tranform** (Module) - Transform used to update the module.
+    * **lr** (float) - Fast adaptation learning rate.
+    * **adapt_transform** (bool, *optional*, default=False) - Whether to update the transform's
+        parameters during fast-adaptation.
+    * **first_order** (bool, *optional*, default=False) - Whether to use the first-order
+        approximation.
+    * **allow_unused** (bool, *optional*, default=None) - Whether to allow differentiation
+        of unused parameters. Defaults to `allow_nograd`.
+    * **allow_nograd** (bool, *optional*, default=False) - Whether to allow adaptation with
+        parameters that have `requires_grad = False`.
+
+    **References**
+
+    1. Finn et al. 2017. “Model-Agnostic Meta-Learning for Fast Adaptation of Deep Networks.”
+    2. Li et al. 2017. “Meta-SGD: Learning to Learn Quickly for Few-Shot Learning.”
+    3. Park & Oliva. 2019. “Meta-Curvature.”
+    4. Behl et al. 2019. “Alpha MAML: Adaptive Model-Agnostic Meta-Learning.”
+    5. Arnold et al. 2019. “When MAML Can Adapt Fast and How to Assist When It Cannot.”
+
+    **Example**
+
+    ~~~python
+    model = SmallCNN()
+    transform = l2l.optim.ModuleTransform(torch.nn.Linear)
+    gbml = l2l.algorithms.GBML(
+        module=model,
+        transform=transform,
+        lr=0.01,
+        adapt_transform=True,
+    )
+    gbml.to(device)
+
+    # Training with 1 adaptation step
+    for iteration in range(10):
+        task_model = gbml.clone()
+        loss = compute_loss(task_model)
+        task_model.adapt(loss)
+        loss.backward()
+    ~~~
     """
 
     def __init__(
