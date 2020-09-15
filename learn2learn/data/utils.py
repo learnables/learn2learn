532686e451d6b459cbf30b6a757e0469fbbc5cd7
seba-1511 <seba-1511@hotmail.com>
2020-09-14 19:45:57 -0700

    Faster data download.

--- learn2learn/data/utils.py
+++ learn2learn/data/utils.py
@@ -2,12 +2,19 @@
 
 import requests
 import tqdm
+import shutil
+
+CHUNK_SIZE = 1 * 1024 * 1024
 
 
-def download_file(source, destination):
-    req = requests.get(source)
+def download_file(source, destination, size=None):
+    req = requests.get(source, stream=True)
     with open(destination, 'wb') as archive:
-        for chunk in tqdm.tqdm(req.iter_content(chunk_size=32768), leave=False):
+        for chunk in tqdm.tqdm(
+            req.iter_content(chunk_size=CHUNK_SIZE),
+            total=size // CHUNK_SIZE,
+            leave=False,
+        ):
             if chunk:
                 archive.write(chunk)
 
