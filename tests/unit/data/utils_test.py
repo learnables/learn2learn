1ac59f469e59d5e149bb57c14ce248fcbc91eaf5
seba-1511 <seba-1511@hotmail.com>
2021-11-06 19:40:58 -0700

    Add tests for l2l.data.utils.

--- tests/unit/data/utils_test.py
+++ tests/unit/data/utils_test.py
@@ -4,6 +4,12 @@ import unittest
 import torch
 import learn2learn as l2l
 
+EPS = 1e-8
+
+
+def close(a, b):
+    return torch.norm(a - b).item() <= EPS
+
 
 class DataUtilsTests(unittest.TestCase):
     def setUp(self):
@@ -13,11 +19,11 @@ class DataUtilsTests(unittest.TestCase):
         pass
 
     def test_partition_task(self):
-        TOTAL_SAMPLES = 20
-        data = torch.randn(TOTAL_SAMPLES, 10)
-        labels = torch.randn(TOTAL_SAMPLES, 1)
-        for shots in range(TOTAL_SAMPLES):
-            queries = TOTAL_SAMPLES - shots
+        NUM_SAMPLES = 20
+        data = torch.randn(NUM_SAMPLES, 10)
+        labels = torch.randint(low=0, high=1, size=(NUM_SAMPLES, ))
+        for shots in range(NUM_SAMPLES):
+            queries = NUM_SAMPLES - shots
             (S_X, S_Y), (Q_X, Q_Y) = l2l.data.partition_task(
                 data,
                 labels,
@@ -27,6 +33,23 @@ class DataUtilsTests(unittest.TestCase):
             self.assertAlmostEquals(shots, S_Y.size(0))
             self.assertAlmostEquals(queries, Q_X.size(0))
             self.assertAlmostEquals(queries, Q_Y.size(0))
+
+    def test_infinite_iterator(self):
+        NUM_SAMPLES = 20
+        data = torch.randn(NUM_SAMPLES, 10)
+        dataset = torch.utils.data.TensorDataset(data)
+        dataloader = torch.utils.data.DataLoader(
+            dataset,
+            shuffle=False,
+            batch_size=1,
+        )
+        inf_dataloader = l2l.data.InfiniteIterator(dataloader)
+        counter = 0
+        for x in inf_dataloader:
+            self.assertTrue(close(x[0], data[counter % NUM_SAMPLES]))
+            if counter > NUM_SAMPLES * 5:
+                break
+            counter += 1
 
 
 if __name__ == "__main__":
