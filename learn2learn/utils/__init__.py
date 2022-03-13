4755a6751ce98fb1918bf9c6ccef6f4cb830c5d0
unknown <pimpalkhutevarad@gmail.com>
2022-03-13 11:46:04 +0530

    fix init by adding super

--- learn2learn/utils/__init__.py
+++ learn2learn/utils/__init__.py
@@ -361,6 +361,7 @@ def flatten_config(args, prefix=None):
 class _ImportRaiser(object):
 
     def __init__(self, name, command):
+        super(_ImportRaiser, self).__init__()
         self.name = name
         self.command = command
 
