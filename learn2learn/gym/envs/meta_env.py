0c37226732818e4f34fb7f06ad6a2cd9707fa19d
unknown <pimpalkhutevarad@gmail.com>
2022-03-13 11:34:40 +0530

    Fix class name in super init

--- learn2learn/gym/envs/meta_env.py
+++ learn2learn/gym/envs/meta_env.py
@@ -30,7 +30,7 @@ class MetaEnv(Env):
     """
 
     def __init__(self, task=None):
-        super(Env, self).__init__()
+        super(MetaEnv, self).__init__()
         if task is None:
             task = self.sample_tasks(1)[0]
         self.set_task(task)
