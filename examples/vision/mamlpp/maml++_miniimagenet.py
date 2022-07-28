88681d05486c9449a426b3a58b4d28ffee6737ae
Theo Morales <theo.morales.fr@gmail.com>
2022-07-28 18:07:51 +0100

    Use the new LSLR transform for the maml++ example

--- examples/vision/mamlpp/maml++_miniimagenet.py
+++ examples/vision/mamlpp/maml++_miniimagenet.py
@@ -19,9 +19,9 @@ import torch
 from collections import namedtuple
 from typing import Tuple
 from tqdm import tqdm
+from learn2learn.optim.transforms.layer_step_lr_transform import PerLayerPerStepLRTransform
 
-from examples.vision.mamlpp.cnn4_bnrs import CNN4_BNRS
-from examples.vision.mamlpp.MAMLpp import MAMLpp
+from learn2learn.vision.models.cnn4 import CNN4
 
 
 MetaBatch = namedtuple("MetaBatch", "support query")
@@ -38,9 +38,9 @@ def accuracy(predictions, targets):
 class MAMLppTrainer:
     def __init__(
         self,
-        ways=5,
-        k_shots=10,
-        n_queries=30,
+        ways=20,
+        k_shots=5,
+        n_queries=5,
         steps=5,
         msl_epochs=25,
         DA_epochs=50,
@@ -52,6 +52,7 @@ class MAMLppTrainer:
         if self._use_cuda and torch.cuda.device_count():
             torch.cuda.manual_seed(seed)
             self._device = torch.device("cuda")
+        print(f"[*] Using device: {self._device}")
         random.seed(seed)
         np.random.seed(seed)
         torch.manual_seed(seed)
@@ -64,15 +65,16 @@ class MAMLppTrainer:
             self._test_tasks,
         ) = l2l.vision.benchmarks.get_tasksets(
             "mini-imagenet",
-            train_samples=k_shots,
+            train_samples=k_shots+n_queries,
             train_ways=ways,
-            test_samples=n_queries,
+            test_samples=k_shots+n_queries,
             test_ways=ways,
             root="~/data",
         )
+        print("[*] Done.")
 
         # Model
-        self._model = CNN4_BNRS(ways, adaptation_steps=steps)
+        self._model = CNN4(ways) # TODO: Change config for miniImageNet (32 filters ?)
         if self._use_cuda:
             self._model.cuda()
 
@@ -109,9 +111,9 @@ class MAMLppTrainer:
         Separate data batch into adaptation/evalutation sets.
         """
         images, labels = batch
-        batch_size = self._k_shots + self._n_queries
-        assert batch_size <= images.shape[0], "K+N are greater than the batch size!"
-        indices = torch.randperm(batch_size)
+        task_size = self._k_shots + self._n_queries
+        assert task_size <= images.shape[0], "K+N are smaller than the batch size!"
+        indices = torch.randperm(task_size)
         support_indices = indices[: self._k_shots]
         query_indices = indices[self._k_shots :]
         return MetaBatch(
@@ -125,7 +127,7 @@ class MAMLppTrainer:
     def _training_step(
         self,
         batch: MetaBatch,
-        learner: MAMLpp,
+        learner: torch.nn.Module,
         msl: bool = True,
         epoch: int = 0,
     ) -> Tuple[torch.Tensor, float]:
@@ -147,26 +149,26 @@ class MAMLppTrainer:
         # Adapt the model on the support set
         for step in range(self._steps):
             # forward + backward + optimize
-            pred = learner(s_inputs, step)
+            pred = learner(s_inputs)
             support_loss = self._inner_criterion(pred, s_labels)
-            learner.adapt(support_loss, first_order=not second_order, step=step)
+            learner.adapt(support_loss, first_order=not second_order)
             # Multi-Step Loss
             if msl:
-                q_pred = learner(q_inputs, step)
+                q_pred = learner(q_inputs)
                 query_loss += self._step_weights[step] * self._inner_criterion(
                     q_pred, q_labels
                 )
 
         # Evaluate the adapted model on the query set
         if not msl:
-            q_pred = learner(q_inputs, self._steps-1)
+            q_pred = learner(q_inputs, inference=True)
             query_loss = self._inner_criterion(q_pred, q_labels)
         acc = accuracy(q_pred, q_labels).detach()
 
         return query_loss, acc
 
     def _testing_step(
-        self, batch: MetaBatch, learner: MAMLpp
+        self, batch: MetaBatch, learner: torch.nn.Module
     ) -> Tuple[torch.Tensor, float]:
         s_inputs, s_labels = batch.support
         q_inputs, q_labels = batch.query
@@ -180,12 +182,12 @@ class MAMLppTrainer:
         # Adapt the model on the support set
         for step in range(self._steps):
             # forward + backward + optimize
-            pred = learner(s_inputs, step)
+            pred = learner(s_inputs)
             support_loss = self._inner_criterion(pred, s_labels)
-            learner.adapt(support_loss, step=step)
+            learner.adapt(support_loss)
 
         # Evaluate the adapted model on the query set
-        q_pred = learner(q_inputs, self._steps-1)
+        q_pred = learner(q_inputs, inference=True)
         query_loss = self._inner_criterion(q_pred, q_labels).detach()
         acc = accuracy(q_pred, q_labels)
 
@@ -195,23 +197,29 @@ class MAMLppTrainer:
         self,
         meta_lr=0.001,
         fast_lr=0.01,
-        meta_bsz=5,
-        epochs=100,
+        meta_bsz=16,
+        epochs=1,
         val_interval=1,
     ):
         print("[*] Training...")
-        maml = MAMLpp(
+        transform = PerLayerPerStepLRTransform(fast_lr, self._steps, self._model)
+        # Setting adapt_transform=True means that the transform will be updated in
+        # the *adapt* function, which is not what we want. We want it to compute gradients during
+        # eval_loss.backward() only, so that it's updated in opt.step().
+        mamlpp = l2l.algorithms.GBML(
             self._model,
-            lr=fast_lr, # Initialisation LR for all layers and steps
-            adaptation_steps=self._steps, # For LSLR
-            first_order=False,
-            allow_nograd=True, # For the parameters of the MetaBatchNorm layers
+            transform,
+            lr=1.0,
+            allow_nograd=True,
+            adapt_transform=False,
+            pass_param_names=True,
         )
-        opt = torch.optim.AdamW(maml.parameters(), meta_lr, betas=(0, 0.999))
+        opt = torch.optim.AdamW(mamlpp.parameters(), meta_lr, betas=(0.9, 0.99))
 
         iter_per_epoch = (
             train_samples // (meta_bsz * (self._k_shots + self._n_queries))
         ) + 1
+        print(f"[*] Training with {iter_per_epoch} iterations/epoch with {train_samples} total training samples")
         scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
             opt,
             T_max=epochs * iter_per_epoch,
@@ -228,7 +236,7 @@ class MAMLppTrainer:
                     meta_batch = self._split_batch(self._train_tasks.sample())
                     meta_loss, meta_acc = self._training_step(
                         meta_batch,
-                        maml.clone(),
+                        mamlpp.clone(),
                         msl=(epoch < self._msl_epochs),
                         epoch=epoch,
                     )
@@ -241,7 +249,7 @@ class MAMLppTrainer:
 
                 # Average the accumulated gradients and optimize
                 with torch.no_grad():
-                    for p in maml.parameters():
+                    for p in mamlpp.parameters():
                         # Remember the MetaBatchNorm layer has parameters that don't require grad!
                         if p.requires_grad:
                             p.grad.data.mul_(1.0 / meta_bsz)
@@ -259,49 +267,50 @@ class MAMLppTrainer:
 
             # ======= Validation ========
             if (epoch + 1) % val_interval == 0:
-                # Backup the BatchNorm layers' running statistics
-                maml.backup_stats()
-
                 # Compute the meta-validation loss
                 # TODO: Go through the entire validation set, which shouldn't be shuffled, and
                 # which tasks should not be continuously resampled from!
+                # This may be done in the get_tasksets() method actually...
                 meta_val_losses, meta_val_accs = [], []
                 for _ in tqdm(range(val_samples // tasks)):
                     meta_batch = self._split_batch(self._valid_tasks.sample())
-                    loss, acc = self._testing_step(meta_batch, maml.clone())
+                    loss, acc = self._testing_step(meta_batch, mamlpp.clone())
                     meta_val_losses.append(loss)
                     meta_val_accs.append(acc)
                 meta_val_loss = float(torch.Tensor(meta_val_losses).mean().item())
                 meta_val_acc = float(torch.Tensor(meta_val_accs).mean().item())
                 print(f"Meta-validation Loss: {meta_val_loss:.6f}")
                 print(f"Meta-validation Accuracy: {meta_val_acc:.6f}")
-                # Restore the BatchNorm layers' running statistics
-                maml.restore_backup_stats()
             print("============================================")
 
-        return self._model.state_dict()
+        return self._model.state_dict(), transform.state_dict()
 
     def test(
         self,
         model_state_dict,
+        trasnform_state_dict,
         meta_lr=0.001,
         fast_lr=0.01,
         meta_bsz=5,
     ):
         self._model.load_state_dict(model_state_dict)
-        maml = MAMLpp(
+        transform = PerLayerPerStepLRTransform(fast_lr, self._steps, self._model)
+        transform.load_state_dict(trasnform_state_dict)
+        # Setting adapt_transform=True means that the transform will be updated in
+        # the *adapt* function, which is not what we want. We want it to compute gradients during
+        # eval_loss.backward() only, so that it's updated in opt.step().
+        mamlpp = l2l.algorithms.GBML(
             self._model,
-            lr=fast_lr,
-            adaptation_steps=self._steps,
-            first_order=False,
-            allow_nograd=True,
+            transform,
+            lr=1.0,
+            adapt_transform=False,
+            pass_param_names=True,
         )
-        opt = torch.optim.AdamW(maml.parameters(), meta_lr, betas=(0, 0.999))
 
         meta_losses, meta_accs = [], []
         for _ in tqdm(range(test_samples // tasks)):
             meta_batch = self._split_batch(self._test_tasks.sample())
-            loss, acc = self._testing_step(meta_batch, maml.clone())
+            loss, acc = self._testing_step(meta_batch, mamlpp.clone())
             meta_losses.append(loss)
             meta_accs.append(acc)
         loss = float(torch.Tensor(meta_losses).mean().item())
@@ -312,6 +321,6 @@ class MAMLppTrainer:
 
 if __name__ == "__main__":
     mamlPlusPlus = MAMLppTrainer()
-    model = mamlPlusPlus.train()
-    mamlPlusPlus.test(model)
+    model_state_dict, transform_state_dict = mamlPlusPlus.train()
+    mamlPlusPlus.test(model_state_dict, transform_state_dict)
 
