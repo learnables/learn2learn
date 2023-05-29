9e50259b07293f5a27e46ffe2ed1335bc28fdc22
Séb Arnold <seba-1511@hotmail.com>
2023-05-28 19:09:30 -0700

    More whitespaces.

--- learn2learn/vision/datasets/fgvc_aircraft.py
+++ learn2learn/vision/datasets/fgvc_aircraft.py
@@ -151,7 +151,7 @@ class FGVCAircraft(Dataset):
             def is_within_directory(directory, target):
                 abs_directory = os.path.abspath(directory)
                 abs_target = os.path.abspath(target)
-                prefix = os.path.commonprefix([abs_directory, abs_target])       
+                prefix = os.path.commonprefix([abs_directory, abs_target])
                 return prefix == abs_directory
 
             def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
