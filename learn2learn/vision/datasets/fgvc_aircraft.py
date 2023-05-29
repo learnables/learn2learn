275010df7a22be8567a85e83339e2402dc9861fd
Séb Arnold <seba-1511@hotmail.com>
2023-05-28 19:06:30 -0700

    Fix more linting.

--- learn2learn/vision/datasets/fgvc_aircraft.py
+++ learn2learn/vision/datasets/fgvc_aircraft.py
@@ -151,7 +151,7 @@ class FGVCAircraft(Dataset):
             def is_within_directory(directory, target):
                 abs_directory = os.path.abspath(directory)
                 abs_target = os.path.abspath(target)
-                prefix = os.path.commonprefix([abs_directory, abs_target])                
+                prefix = os.path.commonprefix([abs_directory, abs_target])       
                 return prefix == abs_directory
 
             def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
@@ -159,7 +159,7 @@ class FGVCAircraft(Dataset):
                     member_path = os.path.join(path, member.name)
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
-                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner)
 
             safe_extract(tar_file, data_path)
         family_names = ['images_family_train.txt',
